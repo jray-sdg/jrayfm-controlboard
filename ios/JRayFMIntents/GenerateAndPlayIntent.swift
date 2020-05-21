dc37e7680e85cc0fe9338eb2c7c319d24b6db886
Jonathan Ray <asky314159@gmail.com>
2020-05-20 23:00:13 -0500

    Help siri know everything is okay

--- ios/JRayFMIntents/GenerateAndPlayIntent.swift
+++ ios/JRayFMIntents/GenerateAndPlayIntent.swift
@@ -10,6 +10,21 @@ import Intents
 
 class GenerateAndPlayIntent : NSObject, INPlayMediaIntentHandling {
     
+    func resolveMediaItems(for intent: INPlayMediaIntent,
+                           with completion: @escaping ([INPlayMediaMediaItemResolutionResult]) -> Void) {
+        // We always take the same action no matter what so everything is a success
+        var result: [INPlayMediaMediaItemResolutionResult] = []
+        if let intentMediaItems = intent.mediaItems {
+            for intentMedia in intentMediaItems {
+                result.append(INPlayMediaMediaItemResolutionResult.success(with: intentMedia))
+            }
+        }
+        else {
+            result.append(INPlayMediaMediaItemResolutionResult.success(with: INMediaItem(identifier: nil, title: nil, type: .music, artwork: nil)))
+        }
+        completion(result)
+    }
+    
     func handle(intent: INPlayMediaIntent, completion: @escaping (INPlayMediaIntentResponse) -> Void) {
         // Always hand execution to the full app
         let response = INPlayMediaIntentResponse(code: .handleInApp, userActivity: nil)
